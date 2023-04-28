b9a53e37f5f3aad3d1eaaf17b3ee7ade6c6d113f
aimakov <nurbolat.aimakov@gmail.com>
2023-04-28 13:31:02 +0900

    Added host display on [id].tsx

--- pages/api/joinRoom.ts
+++ pages/api/joinRoom.ts
@@ -4,33 +4,45 @@ import { supabase } from "@/utils/supabaseClient";
 import { getSession } from "../../utils/get-session";
 import { escape } from "querystring";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    try {
-        console.log(req.body.room_code);
-        // console.log(req);
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    console.log(req.body.room_code);
+    // console.log(req);
 
-        const { data, error } = await supabase.from("rooms").select().eq("room_code", req.body.room_code);
+    const { data, error } = await supabase
+      .from("rooms")
+      .select()
+      .eq("room_code", req.body.room_code);
 
-        if (error) throw error;
+    if (error) throw error;
 
-        console.log(data.length);
+    if (data.length) {
+      const { error } = await supabase
+        .from("rooms")
+        .update({
+          users: [
+            ...(data[0].users ?? []),
+            { user_id: req.body.user_id, user_name: req.body.user_name },
+          ],
+        })
+        .eq("room_code", req.body.room_code);
 
-        if (data.length) {
-            const { error } = await supabase
-                .from("rooms")
-                .update({ users: [...(data[0].users ?? []), { user_id: req.body.user_id, user_name: req.body.user_name }] })
-                .eq("room_code", req.body.room_code);
+      if (error) throw error;
+      // res.status(400).json({ success: false, message: error.message });
 
-            if (error) throw error;
-            // res.status(400).json({ success: false, message: error.message });
-
-            res.status(200).json({ success: true, message: `Room ${data[0].room_name} is joined!` });
-        } else {
-            res.status(400).json({ success: false, message: "Such room doesn't exist." });
-        }
-
-        // console.log(data[0].id);
-    } catch (error) {
-        res.status(400).json({ success: false, message: error.message });
+      res.status(200).json({
+        success: true,
+        message: `Room ${data[0].room_name} is joined!`,
+      });
+    } else {
+      res
+        .status(400)
+        .json({ success: false, message: "Such room doesn't exist." });
     }
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 }
--- pages/join.tsx
+++ pages/join.tsx
@@ -12,72 +12,80 @@ import { supabase } from "@/utils/supabaseClient";
 type Props = {};
 
 const Join = (props: Props) => {
-    const user = useSelector(selectUser);
-    const router = useRouter();
-    const dispatch = useDispatch();
-    const [roomCode, setRoomCode] = useState("");
+  const user = useSelector(selectUser);
+  const router = useRouter();
+  const dispatch = useDispatch();
+  const [roomCode, setRoomCode] = useState("");
 
-    const typeRoomCode = (e: any) => {
-        if (e.target.value.length > 6) return;
-        else setRoomCode(e.target.value);
-    };
+  const typeRoomCode = (e: any) => {
+    if (e.target.value.length > 6) return;
+    else setRoomCode(e.target.value);
+  };
 
-    const joinRoom = async () => {
-        try {
-            const response = await axios.post("api/joinRoom", {
-                room_code: roomCode.toUpperCase(),
-                user_id: user.id,
-                user_name: user.user_metadata.full_name,
-            });
+  const joinRoom = async () => {
+    try {
+      const response = await axios.post("api/joinRoom", {
+        room_code: roomCode.toUpperCase(),
+        user_id: user.id,
+        user_name: user.user_metadata.full_name,
+      });
 
-            dispatch(actionSuccess({ message: response.data.message }));
-            setTimeout(() => {
-                router.push("rooms/" + roomCode.toUpperCase());
-            }, 2000);
-        } catch (error) {
-            dispatch(actionError({ message: error.response.data.message }));
-        }
-    };
+      dispatch(actionSuccess({ message: response.data.message }));
+      setTimeout(() => {
+        router.push("rooms/" + roomCode.toUpperCase());
+      }, 2000);
+    } catch (error) {
+      dispatch(actionError({ message: error.response.data.message }));
+    }
+  };
 
-    const getRoom = async () => {
-        const { data, error } = await supabase.from("rooms").select().eq("room_code", roomCode.toUpperCase());
+  const getRoom = async () => {
+    const { data, error } = await supabase
+      .from("rooms")
+      .select()
+      .eq("room_code", roomCode.toUpperCase());
 
-        if (error) console.log(error);
+    if (error) console.log(error);
+  };
 
-        console.log(data);
-    };
+  useEffect(() => {
+    if (!user.id) {
+      router.push("/");
+      dispatch(actionError({ message: `You are not signed in.` }));
+    }
+  }, [router]);
 
-    useEffect(() => {
-        if (!user.id) {
-            router.push("/");
-            dispatch(actionError({ message: `You are not signed in.` }));
-        }
-    }, [router]);
-
-    if (!user.id) return <Layout> </Layout>;
-    else
-        return (
-            <Layout>
-                <div className="w-full min-h-screen flex flex-col justify-center items-center font-montserrat">
-                    <div className="w-[280px] h-[280px] bg-white/30 rounded-[20px] shadow-md flex flex-col justify-center items-center">
-                        <h2 className="text-2xl font-medium mb-[14px]">Enter Room Code:</h2>
-                        <input value={roomCode} onChange={typeRoomCode} className=" p-4 rounded-[4px] mb-[32px] w-10/12 text-center uppercase text-lg" />
-                        <div className="flex flex-col gap-2">
-                            <button
-                                disabled={roomCode.length < 6}
-                                onClick={joinRoom}
-                                className=" disabled:bg-gray-300 disabled:hover:cursor-not-allowed w-[130px] py-[10px] bg-white/30 rounded-3xl hover:bg-white/50 transition-all"
-                            >
-                                Join Room
-                            </button>
-                            <button onClick={() => router.back()} className="w-[130px] py-[10px] bg-white/30 rounded-3xl hover:bg-white/50 transition-all">
-                                Go Back
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </Layout>
-        );
+  if (!user.id) return <Layout> </Layout>;
+  else
+    return (
+      <Layout>
+        <div className="w-full min-h-screen flex flex-col justify-center items-center font-montserrat">
+          <div className="w-[280px] h-[280px] bg-white/30 rounded-[20px] shadow-md flex flex-col justify-center items-center">
+            <h2 className="text-2xl font-medium mb-[14px]">Enter Room Code:</h2>
+            <input
+              value={roomCode}
+              onChange={typeRoomCode}
+              className=" p-4 rounded-[4px] mb-[32px] w-10/12 text-center uppercase text-lg"
+            />
+            <div className="flex flex-col gap-2">
+              <button
+                disabled={roomCode.length < 6}
+                onClick={joinRoom}
+                className=" disabled:bg-gray-300 disabled:hover:cursor-not-allowed w-[130px] py-[10px] bg-white/30 rounded-3xl hover:bg-white/50 transition-all"
+              >
+                Join Room
+              </button>
+              <button
+                onClick={() => router.back()}
+                className="w-[130px] py-[10px] bg-white/30 rounded-3xl hover:bg-white/50 transition-all"
+              >
+                Go Back
+              </button>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    );
 };
 
 export default Join;
