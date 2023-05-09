1732234660bd0a07211fdbe9424b7bab2049ae82
aimakov <nurbolat.aimakov@gmail.com>
2023-05-09 15:07:47 +0900

    Rearranged the songs list and added hide player button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./app/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            backgroundImage: {
-                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-            },
-            fontFamily: {
-                montserrat: ["montserrat"],
-                comfortaa: ["comfortaa"],
-            },
-            height: {
-                screen: ["100vh /* fallback for Opera, IE and etc. */", "100dvh"],
-            },
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        montserrat: ["montserrat"],
+        comfortaa: ["comfortaa"],
+      },
+      height: {
+        screen: ["100vh /* fallback for Opera, IE and etc. */", "100dvh"],
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
